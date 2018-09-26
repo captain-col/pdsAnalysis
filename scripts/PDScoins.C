58c0e635a24a7965ebf32a69c61940c75ddde21d
Sergey Martynenko <sergey@Sergeys-MacBook-Pro.local>
2018-09-26 08:31:38 -0400

    Bug Fix
    Fix bug with saving only half of entries

--- scripts/PDScoins.C
+++ scripts/PDScoins.C
@@ -213,12 +213,12 @@ void findCoincidence(TTree* input1,TTree* input2,TTree* input3,TTree* output1,TT
     int MAXSAMPLES=2100;
     
     
-    TString* file_name(new TString);
-    unsigned int          event_number_g;
-    int           computer_secIntoEpoch_g;
-    long long         computer_nsIntoSec_g;
-    unsigned int          digitizer_time_one_g;
-    int                   digitizer_dup_one_g;
+    TString* file_name1(new TString);
+    unsigned int          event_number_g1;
+    int           computer_secIntoEpoch_g1;
+    long long         computer_nsIntoSec_g1;
+    unsigned int          digitizer_time_one_g1;
+    int                   digitizer_dup_one_g1;
     std::vector<int>* pmtNum1(new std::vector<int>);
     std::vector<double>* pmtCharge1(new std::vector<double>);
     std::vector<int>* pmtTimeStart1(new std::vector<int>);
@@ -227,15 +227,15 @@ void findCoincidence(TTree* input1,TTree* input2,TTree* input3,TTree* output1,TT
     unsigned short RF_ADC1[2100];
     std::vector<int> *RF_timeStart1(new std::vector<int>);
     std::vector<int> *RF_timeStop1(new std::vector<int>);
-    int aligned12_g;
-    int aligned13_g;
+    int aligned12_g1;
+    int aligned13_g1;
 
-    input1->SetBranchAddress("event_number_global",&event_number_g);
-    input1->SetBranchAddress("file_name",&file_name);
-    input1->SetBranchAddress("computer_secIntoEpoch",&computer_secIntoEpoch_g);
-    input1->SetBranchAddress("computer_nsIntoSec",&computer_nsIntoSec_g);
-    input1->SetBranchAddress("digitizer_time",&digitizer_time_one_g);
-    input1->SetBranchAddress("digitizer_duplicate",&digitizer_dup_one_g);
+    input1->SetBranchAddress("event_number_global",&event_number_g1);
+    input1->SetBranchAddress("file_name",&file_name1);
+    input1->SetBranchAddress("computer_secIntoEpoch",&computer_secIntoEpoch_g1);
+    input1->SetBranchAddress("computer_nsIntoSec",&computer_nsIntoSec_g1);
+    input1->SetBranchAddress("digitizer_time",&digitizer_time_one_g1);
+    input1->SetBranchAddress("digitizer_duplicate",&digitizer_dup_one_g1);
     input1->SetBranchAddress("hit_pmt",&pmtNum1);
     input1->SetBranchAddress("hit_charge",&pmtCharge1);
     input1->SetBranchAddress("hit_timeStart",&pmtTimeStart1);
@@ -244,8 +244,8 @@ void findCoincidence(TTree* input1,TTree* input2,TTree* input3,TTree* output1,TT
     input1->SetBranchAddress("RF_waveform",RF_ADC1);
     input1->SetBranchAddress("RF_timeStart",&RF_timeStart1);
     input1->SetBranchAddress("RF_timeStop",&RF_timeStop1);
-    input1->SetBranchAddress("Alignment_12",&aligned12_g);
-    input1->SetBranchAddress("Alignment_13",&aligned13_g);
+    input1->SetBranchAddress("Alignment_12",&aligned12_g1);
+    input1->SetBranchAddress("Alignment_13",&aligned13_g1);
 
     std::vector<int>* pmtNum2(new std::vector<int>);
     std::vector<double>* pmtCharge2(new std::vector<double>);
@@ -286,44 +286,44 @@ void findCoincidence(TTree* input1,TTree* input2,TTree* input3,TTree* output1,TT
     
     //fill digitizer1
     
-    TString file_name_c;
-    unsigned int          event_number_g_c;
-    int           computer_secIntoEpoch_g_c;
-    long long         computer_nsIntoSec_g_c;
-    int                   digitizer_dup_one_g_c;
-    unsigned int          digitizer_time_one_g_c;
-    std::vector<int> pmtNum_c;
-    std::vector<double> pmtCharge_c;
-    std::vector<int> pmtTimeStart_c;
-    std::vector<int> pmtTimeStop_c;
-    std::vector<int> pmtTimePeak_c;
-    unsigned short RF_ADC_c[2100];
-    std::vector<int> RF_timeStart_c;
-    std::vector<int> RF_timeStop_c;
-    int aligned12_c;
-    int aligned13_c;
+    TString file_name_c1;
+    unsigned int          event_number_g_c1;
+    int           computer_secIntoEpoch_g_c1;
+    long long         computer_nsIntoSec_g_c1;
+    int                   digitizer_dup_one_g_c1;
+    unsigned int          digitizer_time_one_g_c1;
+    std::vector<int> pmtNum_c1;
+    std::vector<double> pmtCharge_c1;
+    std::vector<int> pmtTimeStart_c1;
+    std::vector<int> pmtTimeStop_c1;
+    std::vector<int> pmtTimePeak_c1;
+    unsigned short RF_ADC_c1[2100];
+    std::vector<int> RF_timeStart_c1;
+    std::vector<int> RF_timeStop_c1;
+    int aligned12_c1;
+    int aligned13_c1;
     std::vector<int> coincNum;
     std::vector<int> coincPeakTime;
     std::vector<double> coincCharge;
     std::vector<int> coincPMT;
     std::vector<int> coincdT;
     
-    output1->Branch("event_number_global",&event_number_g_c);
-    output1->Branch("file_name",&file_name_c);
-    output1->Branch("computer_secIntoEpoch",&computer_secIntoEpoch_g_c);
-    output1->Branch("computer_nsIntoSec",&computer_nsIntoSec_g_c);
-    output1->Branch("digitizer_duplicate",&digitizer_dup_one_g_c);
-    output1->Branch("digitizer_time",&digitizer_time_one_g_c);
-    output1->Branch("hit_pmt",&pmtNum_c);
-    output1->Branch("hit_charge",&pmtCharge_c);
-    output1->Branch("hit_timeStart",&pmtTimeStart_c);
-    output1->Branch("hit_timeStop",&pmtTimeStop_c);
-    output1->Branch("hit_timePeak",&pmtTimePeak_c);
-    output1->Branch("RF_waveform",&RF_ADC_c,"RF_ADC[2100]/s");
-    output1->Branch("RF_timeStart",&RF_timeStart_c);
-    output1->Branch("RF_timeStop",&RF_timeStop_c);
-    output1->Branch("Alignment_12",&aligned12_c);
-    output1->Branch("Alignment_13",&aligned13_c);
+    output1->Branch("event_number_global",&event_number_g_c1);
+    output1->Branch("file_name",&file_name_c1);
+    output1->Branch("computer_secIntoEpoch",&computer_secIntoEpoch_g_c1);
+    output1->Branch("computer_nsIntoSec",&computer_nsIntoSec_g_c1);
+    output1->Branch("digitizer_duplicate",&digitizer_dup_one_g_c1);
+    output1->Branch("digitizer_time",&digitizer_time_one_g_c1);
+    output1->Branch("hit_pmt",&pmtNum_c1);
+    output1->Branch("hit_charge",&pmtCharge_c1);
+    output1->Branch("hit_timeStart",&pmtTimeStart_c1);
+    output1->Branch("hit_timeStop",&pmtTimeStop_c1);
+    output1->Branch("hit_timePeak",&pmtTimePeak_c1);
+    output1->Branch("RF_waveform",&RF_ADC_c1,"RF_ADC[2100]/s");
+    output1->Branch("RF_timeStart",&RF_timeStart_c1);
+    output1->Branch("RF_timeStop",&RF_timeStop_c1);
+    output1->Branch("Alignment_12",&aligned12_c1);
+    output1->Branch("Alignment_13",&aligned13_c1);
     output1->Branch("CoincNumber",&coincNum);
     output1->Branch("CoincPeakTime",&coincPeakTime);
     output1->Branch("CoincPMT",&coincPMT);
@@ -331,15 +331,15 @@ void findCoincidence(TTree* input1,TTree* input2,TTree* input3,TTree* output1,TT
     output1->Branch("CoincCharge",&coincCharge);
 
     int entry1 = input1->GetEntries();
-    int n=9;
+
     for(int i=0;i<entry1;++i){
         input1->GetEntry(i);
-        if(aligned12_g==-1 || aligned13_g==-1)continue;
-        if((*RF_timeStart1)[0]==-1)continue;
+
+        if(aligned12_g1!=-1 && aligned13_g1!=-1 && (*RF_timeStart1)[0]!=-1){
         if(i%100==0)
         std::cout<<i<<std::endl;
-        input2->GetEntry(aligned12_g);
-        input3->GetEntry(aligned13_g);
+        input2->GetEntry(aligned12_g1);
+        input3->GetEntry(aligned13_g1);
         std::vector<peak_f> allpeaks;
      
         for(std::size_t pmt=0;pmt<pmtNum1->size();++pmt){
@@ -375,7 +375,7 @@ void findCoincidence(TTree* input1,TTree* input2,TTree* input3,TTree* output1,TT
 #define Coinc
 #ifdef Coinc
         
-        if((int)allpeaks.size()==0)continue;
+            if((int)allpeaks.size()>0){
         std::sort(allpeaks.begin(),allpeaks.end(),[](peak_f l, peak_f r){
             int tl=l.dT;
             int tr=r.dT;
@@ -424,31 +424,6 @@ void findCoincidence(TTree* input1,TTree* input2,TTree* input3,TTree* output1,TT
         
 #endif
         
- 
-        event_number_g_c=event_number_g;
-        file_name_c=(*file_name);
-        computer_secIntoEpoch_g_c= computer_secIntoEpoch_g;
-        computer_nsIntoSec_g_c=computer_nsIntoSec_g;
-        digitizer_time_one_g_c=digitizer_time_one_g;
-        digitizer_dup_one_g_c=digitizer_dup_one_g;
-        for(std::size_t k=0;k<pmtNum1->size();++k){
-            pmtNum_c.push_back((*pmtNum1)[k]);
-            pmtCharge_c.push_back((*pmtCharge1)[k]);
-            pmtTimeStart_c.push_back((*pmtTimeStart1)[k]);
-            pmtTimeStop_c.push_back((*pmtTimeStop1)[k]);
-            pmtTimePeak_c.push_back((*pmtTimePeak1)[k]);
-        }
-        for(std::size_t k=0;k<RF_timeStart1->size();++k){
-            RF_timeStart_c.push_back((*RF_timeStart1)[k]);
-            RF_timeStop_c.push_back((*RF_timeStop1)[k]);
-        }
-        
-        for(int k=0;k<2100;++k){
-            RF_ADC_c[k]=RF_ADC1[k];
-        }
-        
-            aligned12_c=aligned12_g;
-            aligned13_c=aligned13_g;
             
             for(std::size_t k=0;k<coincidences.size();++k){
                 for(std::size_t l=0;l<coincidences[k].peaks.size();++l){
@@ -460,6 +435,50 @@ void findCoincidence(TTree* input1,TTree* input2,TTree* input3,TTree* output1,TT
                 }
             }
             
+            }else{
+                coincNum.push_back(-9999);
+                coincPeakTime.push_back(-9999);
+                coincPMT.push_back(-9999);
+                coincdT.push_back(-9999);
+                coincCharge.push_back(-9999);
+            }
+        allpeaks.clear();
+        }else{
+            
+            coincNum.push_back(-9999);
+            coincPeakTime.push_back(-9999);
+            coincPMT.push_back(-9999);
+            coincdT.push_back(-9999);
+            coincCharge.push_back(-9999);
+            
+
+        }
+    
+        
+        event_number_g_c1=event_number_g1;
+        file_name_c1=(*file_name1);
+        computer_secIntoEpoch_g_c1= computer_secIntoEpoch_g1;
+        computer_nsIntoSec_g_c1=computer_nsIntoSec_g1;
+        digitizer_time_one_g_c1=digitizer_time_one_g1;
+        digitizer_dup_one_g_c1=digitizer_dup_one_g1;
+        for(std::size_t k=0;k<pmtNum1->size();++k){
+            pmtNum_c1.push_back((*pmtNum1)[k]);
+            pmtCharge_c1.push_back((*pmtCharge1)[k]);
+            pmtTimeStart_c1.push_back((*pmtTimeStart1)[k]);
+            pmtTimeStop_c1.push_back((*pmtTimeStop1)[k]);
+            pmtTimePeak_c1.push_back((*pmtTimePeak1)[k]);
+        }
+        for(std::size_t k=0;k<RF_timeStart1->size();++k){
+            RF_timeStart_c1.push_back((*RF_timeStart1)[k]);
+            RF_timeStop_c1.push_back((*RF_timeStop1)[k]);
+        }
+        
+        for(int k=0;k<2100;++k){
+            RF_ADC_c1[k]=RF_ADC1[k];
+        }
+        
+        aligned12_c1=aligned12_g1;
+        aligned13_c1=aligned13_g1;
         
         
         output1->Fill();
@@ -468,19 +487,18 @@ void findCoincidence(TTree* input1,TTree* input2,TTree* input3,TTree* output1,TT
         coincPMT.clear();
         coincdT.clear();
         coincCharge.clear();
-        pmtNum_c.clear();
-        pmtCharge_c.clear();
-        pmtTimeStart_c.clear();
-        pmtTimeStop_c.clear();
-        pmtTimePeak_c.clear();
-        RF_timeStart_c.clear();
-        RF_timeStop_c.clear();
-
-        allpeaks.clear();
-       
+        pmtNum_c1.clear();
+        pmtCharge_c1.clear();
+        pmtTimeStart_c1.clear();
+        pmtTimeStop_c1.clear();
+        pmtTimePeak_c1.clear();
+        RF_timeStart_c1.clear();
+        RF_timeStop_c1.clear();
+        
+        
     }
 
-    delete file_name;
+    delete file_name1;
     delete pmtNum1;
     delete pmtCharge1;
     delete pmtTimeStart1;
@@ -505,7 +523,7 @@ void findCoincidence(TTree* input1,TTree* input2,TTree* input3,TTree* output1,TT
     copyTree(input2,output2,1);
     copyTree(input3,output3,1);
     
-    
+   
     
 }
 
@@ -531,12 +549,12 @@ int PDScoins(){
     
     TTree *digit_two_new = new TTree("digitizer2","second digitizer data");
     
-    
     TTree *digit_three_new = new TTree("digitizer3","third digitizer data");
 
     findCoincidence(digit_one,digit_two,digit_three,digit_one_new,digit_two_new,digit_three_new);
-    
-    
+    f_new->Write();
+    std::cout<<"entry1="<<digit_one->GetEntries()<<" ;entry2="<<digit_two->GetEntries()<<" ;entry3="<<digit_three->GetEntries()<<std::endl;
+    std::cout<<"entry1_new="<<digit_one_new->GetEntries()<<" ;entry2_new="<<digit_two_new->GetEntries()<<" ;entry3_new="<<digit_three_new->GetEntries()<<std::endl;
 
     return 0;
 }
